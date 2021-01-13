b9edd76fa5eff6e4ed01fd855a66471f3396b82f
Hein <hein@5degrees.ch>
2021-01-13 23:12:37 +0630

    Vue3 Todo

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="bg-purple-600 text-white">
+    <div class="container mx-auto">
+      <div class="flex items-center">
+        <router-link to="/"
+                     class="inline-block px-16 py-4 transition-colors hover:bg-purple-500 duration-300">Home</router-link>
+        <router-link to="/todos"
+                     class="inline-block px-16 py-4 transition-colors hover:bg-purple-500 duration-300">Todos</router-link>
+      </div>
+    </div>
+  </nav>
+
+  <!-- 
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div> -->
+  <router-view/>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container bg-white-600 mx-auto">
+        <h1 class="mt-8 text-2xl">The Home Page</h1>
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+    
+  }
+}
+</script>
